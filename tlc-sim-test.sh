b031cfd7646e3b37175aad44a881485f82a9ce31
will62794 <will627@comcast.net>
2019-12-22 15:37:37 +0000

    Pass duration on command line

--- tlc-sim-test.sh
+++ tlc-sim-test.sh
@@ -9,14 +9,14 @@ statsfile="stats.csv"
 printf "" > $statsfile
 printf "workers,states_per_min\n" >> $statsfile
 tlcjar="tla2tools-faster-progress.jar"
+duration=$1
 
 # Run simulator for several seconds for different worker counts.
 # Collect state throughput stats for each.
 for w in {1,2,4,8,12,16,20,24,28,32,36,40,44,48}; do
-  duration="60s"
   depth="50"
-  echo "Running simulation mode with $w workers for duration of $duration"
-  timeout -s SIGTERM $duration java -cp $tlcjar tlc2.TLC -seed 0 -simulate -depth $depth -workers $w -config MC.cfg MC.tla | tee tlc.out
+  echo "Running simulation mode with $w workers for duration of $duration and depth $depth"
+  timeout -s SIGTERM $duration java -cp $tlcjar tlc2.TLC -seed 0 -simulate -depth $depth -workers $w -config MC.cfg MC.tla > tlc.out
   printf $w >> $statsfile 
   printf "," >> $statsfile
   throughput=`grep "states.min" tlc.out | tail -1`
